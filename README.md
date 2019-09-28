b84fe2f68a2bd0efdf593c8bf46854a85a7d15f8
weipengsong <sdswp@126.com>
2019-09-29 02:26:30 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VolumeRender
+
+Developed with Unreal Engine 4
---
+++ Source/VolumeRender/VolumeRenderGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VolumeRenderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOLUMERENDER_API AVolumeRenderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
